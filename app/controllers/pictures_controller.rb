9794855b55565c104574b086703bf230b8e3c63c
ShuheiOhno <inu.suika8@gmail.com>
2020-09-10 16:03:49 +0900

    タグ機能
    #What
    タグ機能

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -16,18 +16,46 @@ class PicturesController < ApplicationController
   def new
     @picture = Picture.new
     @pictures = @place.pictures
-    
+    @tag = Tag.new
   end
 
   def create
-    @picture = @place.pictures.new(picture_params)
-    if @picture.save
-      redirect_to @place
-    else
-      @pictures = @place.pictures
-      flash.now[:alert] = 'メッセージを入力してください。'
-      render :new
-    end
+    # 順番入れ替え不可
+    
+    # tag_name = params[:picture][:name]
+    # tag = Tag.create(name: tag_name)
+    
+    # picture = Picture.new(picture_params)
+    # picture.save
+
+    # PictureTag.create(picture_id: picture.id, tag_id: tag.id)
+  
+    tag_name = params[:picture][:name]
+    tag_name1 = params[:picture][:name1]
+    tag_name2 = params[:picture][:name2]
+    tag = Tag.create(name: tag_name)
+    tag1 = Tag.create(name: tag_name1)
+    tag2 = Tag.create(name: tag_name2)
+    
+    picture = Picture.new(picture_params)
+    picture.save
+
+    PictureTag.create(picture_id: picture.id, tag_id: tag.id)
+    PictureTag.create(picture_id: picture.id, tag_id: tag1.id)
+    PictureTag.create(picture_id: picture.id, tag_id: tag2.id)
+
+
+
+
+    # @picture = @place.pictures.new(picture_params)
+    
+    # if @picture.save
+    #   redirect_to @place
+    # else
+    #   @pictures = @place.pictures
+    #   flash.now[:alert] = 'メッセージを入力してください。'
+    #   render :new
+    # end
   end
 
   private
